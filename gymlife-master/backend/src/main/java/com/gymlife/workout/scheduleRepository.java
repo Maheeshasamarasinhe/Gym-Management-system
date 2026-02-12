fce640ce8544fa1c5f9cdd77d3a8c7aeade5dfaf
Maheesha <maheeshamihiran2021@gmail.com>
2026-02-12 22:50:42 +0530

    implement exersice

--- gymlife-master/backend/src/main/java/com/gymlife/workout/scheduleRepository.java
+++ gymlife-master/backend/src/main/java/com/gymlife/workout/scheduleRepository.java
@@ -1,5 +1,14 @@
 package com.gymlife.workout;
 
-public interface scheduleRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+public interface scheduleRepository extends JpaRepository<schedule, Integer> {
+
+    List<schedule> findByMemberId(Integer memberId);
+
+    void deleteByMemberId(Integer memberId);
 }
